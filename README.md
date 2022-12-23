8d81fb8db086f5029ef9249f335ea12b73e38e1e
Stephane_git <devchkstephane@gmail.com>
2022-12-23 16:50:57 +0300

    first release

---
+++ README.md
@@ -0,0 +1,39 @@
+# API for removing background from image
+
+## Requirements
+
+ - Python >= 3.8
+ - Upgrade your pip
+
+```sh
+pip install --upgrade pip
+```
+ - Install dependancies of the project
+
+```sh
+pip install rembg
+```
+
+  - To run the project: 
+```sh
+python3.8 main.py
+```
+  - To run the project with gunicorn: 
+```sh
+gunicorn --access-logfile app.log main:app -b 0.0.0.0:9090
+```
+
+## API Reference
+
+#### Remove background from image
+
+```http
+  GET /rm-bg
+```
+
+| Parameter | Type            | Description                 |
+| :-------- | :-------------- | :-------------------------- |
+| `token`   | **Text format** | **Required** token          |
+| `files`   | **Files**       | **Required** the image file |
+
+
---
+++ main.py
@@ -0,0 +1,59 @@
+#!flask/bin/python
+from rembg import remove
+import cv2
+import re
+from flask import Flask,request,make_response,send_file
+import json
+import os
+from dotenv import load_dotenv
+import numpy as np
+
+app = Flask(__name__)
+app.debug = True
+
+load_dotenv()
+
+UPLOAD_FOLDER = 'input'
+OUTPUT_FOLDER = 'output'
+TOKEN = os.getenv('TOKEN')
+
+def slugify(s):
+  s = s.lower().strip()
+  s = re.sub(r'[^\w\s-]', '', s)
+  s = re.sub(r'[\s_-]+', '-', s)
+  s = re.sub(r'^-+|-+$', '', s)
+  return s
+
+
+@app.route('/rm-bg', methods=['GET'])
+def remove_background():
+    data = request.files
+    param = request.form
+    if data:
+        if param['token'] == TOKEN:
+            file = data['files']
+            if len(file.filename) > 0:
+                if not os.path.exists(UPLOAD_FOLDER) or not os.path.isdir(UPLOAD_FOLDER):
+                    os.makedirs(UPLOAD_FOLDER)
+                if not os.path.exists(OUTPUT_FOLDER) or not os.path.isdir(OUTPUT_FOLDER):
+                    os.makedirs(OUTPUT_FOLDER)
+                name_file,ext = os.path.splitext(file.filename)
+                filename_slug = slugify(name_file)
+                filename_slug += ext
+                input_path = os.path.join(UPLOAD_FOLDER, filename_slug)
+                file.save(input_path)
+                input = cv2.imread(input_path)
+                output = remove(input)
+                output_path = os.path.join(OUTPUT_FOLDER,'output'+str(ext))
+                cv2.imwrite(output_path, output)
+                os.remove(input_path)
+                return send_file(output_path, mimetype='image/'+str(ext)[1:])
+            else:
+                return make_response(json.dumps({'code':400,'status':'Failed','msg':'No file selected 2'}),400)
+        else:
+            return make_response(json.dumps({'code':400,'status':'Failed','msg':'No access'}),400)
+    else:
+        return make_response(json.dumps({'code':400,'status':'Failed','msg':'No file selected 1'}),400)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9090)
